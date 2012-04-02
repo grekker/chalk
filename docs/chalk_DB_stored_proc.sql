daf32e5d7aab5c4c64a918238658aab8071c2341
matt.chinnock <mattchinnock@gmail.com>
2012-04-02 19:46:48 -0400

    Bug fix

--- docs/chalk_DB_stored_proc.sql
+++ docs/chalk_DB_stored_proc.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS 'users', 'users_level';
+DROP TABLE IF EXISTS `ck_users`, `ck_users_level`;
 
 CREATE TABLE `ck_users_level` (
   `ULID` int(10) unsigned NOT NULL auto_increment PRIMARY KEY,
@@ -16,5 +16,5 @@ CREATE TABLE  `ck_users` (
    CONSTRAINT levelFK FOREIGN KEY (ULID) REFERENCES ck_users_level(ULID) ON DELETE CASCADE
 );
 
-INSERT INTO ck_users_level (levelName) VALUES ('green');
-INSERT INTO ck_users (firstName, lastName, userName, passwd, email, ULID) VALUES ('Dave', 'Tester', 'dtester', '123pass', 'dtester@gmail.com', 1);
+INSERT INTO ck_users_level (levelName) VALUES (`green`);
+INSERT INTO ck_users (firstName, lastName, userName, passwd, email, ULID) VALUES (`Dave`, `Tester`, `dtester`, `123pass`, `dtester@gmail.com`, 1);
