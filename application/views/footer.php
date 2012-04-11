a4f9292edd6c7c1e7630a8bfca25d011d59d36cf
Nathan Phillips <nathan@nathanphillips.net>
2012-04-11 16:00:31 -0400

    Added page templates.

---
+++ application/views/footer.php
@@ -0,0 +1,29 @@
+<footer id="footer">
+        <p>&copy; 2012 Time Lemmings Inc. </p>
+    </footer>
+  </div>
+
+
+  <!-- JavaScript at the bottom for fast page loading -->
+
+  <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.2.min.js"><\/script>')</script>
+
+  <!-- scripts concatenated and minified via build script -->
+  <script src="js/plugins.js"></script>
+  <script src="js/script.js"></script>
+  <script src="js/libs/jquery-ui.widget.min.js"></script>
+  <script src="js/rwd-table.js"></script>
+  <!-- end scripts -->
+
+  <!-- Asynchronous Google Analytics snippet. Change UA-XXXXX-X to be your site's ID.
+       mathiasbynens.be/notes/async-analytics-snippet -->
+  <script>
+    var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+    s.parentNode.insertBefore(g,s)}(document,'script'));
+  </script>
+</body>
+</html>
---
+++ application/views/sidebar.php
@@ -0,0 +1,7 @@
+<div id="sidebar">
+      <h2>Sample Sidebar</h2>
+      <hr />
+      <span>Content Management Systems</span>
+      <hr />
+      <p>Bacon ipsum dolor sit amet pork loin meatball eiusmod chuck pork speck. Fugiat short ribs drumstick rump, fatback elit cupidatat id. Beef ribs in consectetur kielbasa, salami sed reprehenderit pancetta. Officia dolore pariatur, irure short loin sed pancetta esse shankle frankfurter leberkas. Hamburger in short ribs drumstick t-bone, venison chicken ut dolore bresaola corned beef eiusmod commodo. Short loin ribeye in turkey sirloin.</p>
+    </div>
