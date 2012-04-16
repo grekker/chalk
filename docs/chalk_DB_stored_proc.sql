e38b1d1f3ba7a8448858c676ce45d338bc2b70b1
Matt Chinnock <mattchinnock@mattchinnock.local>
2012-04-16 19:41:39 -0400

    updated stored procedure

--- docs/chalk_DB_stored_proc.sql
+++ docs/chalk_DB_stored_proc.sql
@@ -1,20 +1,53 @@
-DROP TABLE IF EXISTS `ck_users`, `ck_users_level`;
+DROP TABLE IF EXISTS  `users_courses`, `submissions`, `assignments`, `courses`, `users`, `users_level`;
 
-CREATE TABLE `ck_users_level` (
-  `ULID` int(10) unsigned NOT NULL auto_increment PRIMARY KEY,
-  `levelName` varchar(200) NOT NULL
+CREATE TABLE `users_level` (
+ 	 `userLevelID` int(10) unsigned NOT NULL auto_increment PRIMARY KEY,
+ 	 `levelName` varchar(200) NOT NULL
 );
 
-CREATE TABLE  `ck_users` (
-  `uid` int(10) unsigned NOT NULL auto_increment PRIMARY KEY,
-  `firstName` varchar(50) NOT NULL,
-  `lastName` varchar(50) NOT NULL,
-  `userName` varchar(50) NOT NULL,
-  `passwd` varchar(150) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `ULID` int(10) unsigned NOT NULL,
-   CONSTRAINT levelFK FOREIGN KEY (ULID) REFERENCES ck_users_level(ULID) ON DELETE CASCADE
+CREATE TABLE  `users` (
+  	`userID` int(10) unsigned NOT NULL auto_increment PRIMARY KEY,
+  	`firstName` varchar(50) NOT NULL,
+  	`lastName` varchar(50) NOT NULL,
+ 	`passwd` varchar(150) NOT NULL,
+ 	`email` varchar(100) NOT NULL,
+ 	`userLevelID` int(10) unsigned NOT NULL,
+ 	 CONSTRAINT levelFK FOREIGN KEY (userLevelID) REFERENCES users_level(userLevelID) ON DELETE CASCADE
 );
 
-INSERT INTO ck_users_level (levelName) VALUES (`green`);
-INSERT INTO ck_users (firstName, lastName, userName, passwd, email, ULID) VALUES (`Dave`, `Tester`, `dtester`, `123pass`, `dtester@gmail.com`, 1);
+CREATE TABLE `courses`(
+	`courseID` int(10) unsigned NOT NULL auto_increment PRIMARY KEY,
+	`courseCode` varchar(50) NOT NULL,
+	`startDate` date NOT NULL,
+	`endDate` date NOT NULL
+);
+
+CREATE TABLE `assignments`(
+	`assignmentID` int(10) unsigned NOT NULL auto_increment PRIMARY KEY,
+	`dueDate` date NOT NULL,
+	`setDate` date NOT NULL,
+	`courseID` int(10) unsigned NOT NULL,
+ 	 CONSTRAINT coursesFK FOREIGN KEY (courseID) REFERENCES courses(courseID) ON DELETE CASCADE
+);
+
+
+CREATE TABLE `submissions`(
+	`submissionID` int(10) unsigned NOT NULL auto_increment PRIMARY KEY,
+	`submissionDate` date NOT NULL,
+	`grade` float,
+	`userID` int(10) unsigned NOT NULL,
+	`assignmentID` int(10) unsigned NOT NULL,
+	CONSTRAINT userIdFK FOREIGN KEY (userID) references users(userID) ON DELETE CASCADE,
+	CONSTRAINT assignmentIdFK FOREIGN KEY (assignmentID) references assignments(assignmentID) ON DELETE CASCADE
+);
+
+CREATE TABLE `users_courses`(
+	`userID` int(10) unsigned NOT NULL,
+	`courseID` int(10) unsigned NOT NULL,
+	CONSTRAINT usersidfkb FOREIGN KEY (userID) references users(userID) ON DELETE CASCADE,
+	CONSTRAINT courseIdFK FOREIGN KEY (courseID) references courses(courseID) ON DELETE CASCADE,
+	PRIMARY KEY(userID, courseID)
+);
+
+INSERT INTO users_level (levelName) VALUES ('green');
+INSERT INTO users (firstName, lastName, passwd, email, userLevelID) VALUES ('Dave', 'Tester', '123pass', 'dtester@gmail.com', 1);
