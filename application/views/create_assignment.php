93e570aab052f00e9a535ce4619a139bd2dfb529
Justin Schuler <schulerj@duq.edu>
2012-04-19 22:35:52 -0400

    Rearranged forms

--- application/views/create_assignment.php
+++ application/views/create_assignment.php
@@ -2,10 +2,10 @@
 <div role="main" id="main">
 	<h2>Create Assignment</h2>
 	<div class="table-wrapper">
-	      <form>
+	  <form>
         <label>Assignment Name <input type="text" /></label>
-        <label>Point Value <input type="number" /></label>
         <label>Due <input type="datetime" /><label>
+        <label>Point Value <input type="number" /></label>
         <label for="desc">Description</label>
         <textarea name="description"></textarea>
         <input type="submit" name="submit" value="submit" />
