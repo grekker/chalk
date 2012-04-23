6678833db071784021f70b497e3dec4baba0317a
Justin Schuler <schulerj@duq.edu>
2012-04-23 00:05:40 -0400

    Teacher/Student Assignments created

---
+++ application/views/view_all_assignments.php
@@ -0,0 +1,42 @@
+<?php include('header.php'); ?>
+<div role="main" id="main">
+	<h2>View All Assignments</h2>
+	<div class="table-wrapper">
+		<table class="respond">
+			<thead>
+				<tr>
+					<th>Assignment Name</th>
+					<th>Due Date</th>
+					<th>Completed/Total</th>
+					<th>Days Late</th>
+					<th>Start Grading</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>Test Assignment 1</td>
+					<td>04/23/11</td>
+					<td>1/1</td>
+					<td>0</td>
+					<td>Grade</td>
+				</tr>
+				<tr>
+					<td>Test Assignment 2</td>
+					<td>04/23/11</td>
+					<td>1/1</td>
+					<td>0</td>
+					<td>Grade</td>
+				</tr>
+				<tr>
+					<td>Test Assignment 3</td>
+					<td>04/23/11</td>
+					<td>1/1</td>
+					<td>0</td>
+					<td>Grade</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</div>
+<?php include('sidebar.php'); ?>
+<?php include('footer.php'); ?>
