0d498a97012e7aec19a8707f75171ad631eb1109
Matt Chinnock <mattchinnock@mattchinnock.local>
2012-04-25 19:09:33 -0400

    changed stored proc to original

--- docs/chalk_DB_stored_proc.sql
+++ docs/chalk_DB_stored_proc.sql
@@ -41,18 +41,18 @@ CREATE TABLE `submissions`(
 	`grade` double,
 	`comments` text,
 	`attachedFiles` text,
-	`email` varchar(100) NOT NULL,
+	`userID` int(10) unsigned NOT NULL,
 	`assignmentID` int(10) unsigned NOT NULL,
-	CONSTRAINT userIdFK FOREIGN KEY (email) references users(email) ON DELETE CASCADE,
+	CONSTRAINT userIdFK FOREIGN KEY (userID) references users(userID) ON DELETE CASCADE,
 	CONSTRAINT assignmentIdFK FOREIGN KEY (assignmentID) references assignments(assignmentID) ON DELETE CASCADE
 );
 
 CREATE TABLE `users_courses`(
-	`email` varchar(100) NOT NULL,
+	`userID` int(10) unsigned NOT NULL,
 	`courseID` int(10) unsigned NOT NULL,
-	CONSTRAINT usersidfkb FOREIGN KEY (email) references users(email) ON DELETE CASCADE,
+	CONSTRAINT usersidfkb FOREIGN KEY (userID) references users(userID) ON DELETE CASCADE,
 	CONSTRAINT courseIdFK FOREIGN KEY (courseID) references courses(courseID) ON DELETE CASCADE,
-	PRIMARY KEY(email, courseID)
+	PRIMARY KEY(userID, courseID)
 );
 
 INSERT INTO users_level (levelName) VALUES ('student');
@@ -61,11 +61,11 @@ INSERT INTO courses (courseName, courseCode, startDate, endDate) VALUES ('Conten
 INSERT INTO users (firstName, lastName, passwd, email, userLevelID) VALUES ('Dave', 'Tester', 'student', 'student@gmail.com', 1);
 INSERT INTO users (firstName, lastName, passwd, email, userLevelID) VALUES ('Cody', 'Eggnitor', 'student', 'student@duq.edu', 1);
 INSERT INTO users (firstName, lastName, passwd, email, userLevelID) VALUES ('Jo', 'Mamma', 'teacher', 'teacher@gmail.com', 2);
-INSERT INTO users_courses (email, courseID) VALUES ('student@gmail.com', 1);
-INSERT INTO users_courses (email, courseID) VALUES ('student@duq.edu', 1);
-INSERT INTO users_courses (email, courseID) VALUES ('teacher@gmail.com', 1);
+INSERT INTO users_courses (userID, courseID) VALUES (1, 1);
+INSERT INTO users_courses (userID, courseID) VALUES (2, 1);
+INSERT INTO users_courses (userID, courseID) VALUES (3, 1);
 INSERT INTO assignments (dueDate, setDate, title, information, maxPoints, courseID) VALUES ('2012-4-28 11:00:00', '2012-4-01 16:40:00', 'Final Project', 'Take everything that you''ve learned up to this point and create your own CMS. This CMS should be able to 1) Store data effectively, 2) Present its content beautifully, 3) Cover all 17 components of a CMS. Extra credit is given if your CMS serves coffee or tea.', 100, 1);
-INSERT INTO submissions (submissionDate, grade, comments, email, assignmentID) VALUES ('2012-4-28 1:30:00', '93', 'Overall, good project. Needs more cross-browser support, though. The green tea was a plus.', 'student@gmail.com', 1);
+INSERT INTO submissions (submissionDate, grade, comments, userID, assignmentID) VALUES ('2012-4-28 1:30:00', '93', 'Overall, good project. Needs more cross-browser support, though. The green tea was a plus.', 1, 1);
 INSERT INTO assignments (dueDate, setDate, title, information, maxPoints, courseID) VALUES ('2012-4-25 16:40:00', '2012-3-25', 'Midterm paper on your personal project', 'Write a paper about the content that you plan to put into your CMS. Your goal here is to focus on the content itself--what do you want your users to take away from your site? If they could describe your site in one paragraph, what would they write?', 50, 1);
-INSERT INTO submissions (submissionDate, grade, email, assignmentID) VALUES ('2012-4-25 16:39:47', '50', 'student@gmail.com', 2);
-INSERT INTO submissions (submissionDate, grade, comments, email, assignmentID) VALUES ('2012-4-27 15:12:15', '45', 'Excellent paper, but I had to deduct points since it was late.','student@duq.edu', 2);
+INSERT INTO submissions (submissionDate, grade, userID, assignmentID) VALUES ('2012-4-25 16:39:47', '50', 1, 2);
+INSERT INTO submissions (submissionDate, grade, comments, userID, assignmentID) VALUES ('2012-4-27 15:12:15', '45', 'Excellent paper, but I had to deduct points since it was late.',2, 2);
